6dbe05d060e51fbf2cf57b819980ce061623b778
Mohit Nagpal <nagpalm7@gmail.com>
2020-12-19 23:04:29 +0530

    Fix alpha issue

--- app.py
+++ app.py
@@ -27,16 +27,6 @@ def post_something():
         path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         file.save(path)
         img = cv2.imread(path, -1)
-        # Convert the image into grayscale image
-        alpha = img[:,:,3]
-        rgb = img[:,:,:-1]
-        gray = cv2.cvtColor(rgb, cv2.COLOR_RGB2GRAY)
-        rgb_gray = cv2.cvtColor(gray, cv2.COLOR_GRAY2RGB)
-        alpha = np.reshape(alpha, (*alpha.shape,1))
-        gray_alpha = np.concatenate([rgb_gray, alpha], axis=2)
-        # Blur the image using Gaussian Blur 
-        gray_blur = cv2.GaussianBlur(gray_alpha, (31, 31), 0)
-
         scale = 250.0
         if request.form.get('scale'):
             try:
@@ -46,9 +36,20 @@ def post_something():
                     "Error": "Please set an float scale between 1 to 300",
                 })
                 return msg , status.HTTP_400_BAD_REQUEST
+        if img.shape[2] > 3:
+            alpha = img[:,:,3]
+            rgb = img[:,:,:-1]
+            gray = cv2.cvtColor(rgb, cv2.COLOR_RGB2GRAY)
+            rgb_gray = cv2.cvtColor(gray, cv2.COLOR_GRAY2RGB)
+            alpha = np.reshape(alpha, (*alpha.shape,1))
+            gray_alpha = np.concatenate([rgb_gray, alpha], axis=2)
+            gray_blur = cv2.GaussianBlur(gray_alpha, (31, 31), 0)
+            cartoon = cv2.divide(gray_alpha, gray_blur, scale=scale)
+        else:
+            gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+            gray_blur = cv2.GaussianBlur(gray, (31, 31), 0)
+            cartoon = cv2.divide(gray, gray_blur, scale=scale)
 
-        # Convert the image into pencil sketch
-        cartoon = cv2.divide(gray_alpha, gray_blur, scale=scale)
         cv2.imwrite(path, cartoon)
         return send_file(path, mimetype='image/png')
     else:
@@ -56,15 +57,6 @@ def post_something():
             "Error": "Please upload a valid png, jpg or jpeg file.",
         })
         return msg , status.HTTP_400_BAD_REQUEST
-
-    if True:
-        return jsonify({
-            "Message": f"Welcome to our awesome platform!!",
-        })
-    else:
-        return jsonify({
-            "ERROR": "Invalid File"
-        })
 
 # A welcome message to test our server
 @app.route('/')
